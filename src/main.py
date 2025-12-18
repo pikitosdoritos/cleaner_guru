3ea2c6a41f4dcbb8539e5a413927bf8527b368ff
pikitosdoritos <fastovetsn@gmail.com>
2025-12-18 18:40:39 +0200

    Add functionality to identify and manage exact duplicate photos
    - Introduced a new JSON output file (result.json) to store information about exact duplicates found in the photo directory.
    - Implemented logic in the duplicates module to find and categorize exact duplicates, retaining one copy and suggesting deletions for others.
    - Updated the hashing module to support the new duplicate detection features.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from tqdm import tqdm
+import json
 from cleaner.scanner import scan_photos
+from cleaner.duplicates import find_exact_duplicates
 
 def main():
     # Знаходимо дирикторію з фотографіями і робимо її шлях абсолютним
@@ -11,18 +13,43 @@ def main():
         print(f"\nPhotos directory {photos_dir} not found")
         return
     
-    # Створюємо лічильники в які будуть записуватись результати(кількість знайдених фото, сумарни1 розмір усіх фото)
-    count = 0
-    total_bytes = 0
+    photos = []
 
     # Проходимо по всіх фото і обгортаємо в прогрес бар
     for photo in tqdm(scan_photos(photos_dir), desc = "Scaning photos..."):
-        count += 1
-        total_bytes += photo.size_bytes
+        photos.append(photo)
+
+    duplicate_groups = find_exact_duplicates(photos)
 
     # Виводимо результат
     print("\nReady!")
-    print(f"Found {count} photos with total size {total_bytes / (1024 ** 3): .2f} GB")
+    print(f"Found {len(photos)} photos")
+    print(f"Found {len(duplicate_groups)} duplicate groups")
+
+    # Створили список для зберження результатів
+    result = []
+
+    # Проходимо по всіх групах
+    for group in duplicate_groups:
+        # Сортуємо групу за розміром
+        sorted_group = sorted(group, key = lambda p: p.size_bytes, reverse = True)
+        # Залишаємо найбільшу по розміру фотографію
+        keep = sorted_group[0].path
+        # Видаляємо всі крім першої
+        delete = [p.path for p in sorted_group[1:]]
+
+        result.append({
+            "type": "exact_duplicates",
+            "keep": keep,
+            "suggest_delete": delete,
+            "count": len(sorted_group)
+        })
+
+        # Зберігаємо результат у JSON-файл в форматі utf-8
+        with open("result.json", "w", encoding = "utf-8") as f:
+            json.dump(result, f, indent = 2, ensure_ascii = False)
+
+        print("Результат збережено у result.json")
 
 if __name__ =="__main__":
     main()
